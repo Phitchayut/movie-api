b80689fd41327fd840a2bddf9379235a5ca75937
Phitchayut <beam.phitchayut@gmail.com>
2022-03-19 22:28:06 +0700

    setFilterGenre

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Movies from './components/Movies';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Filter from "./components/Filter";
+import Movies from "./components/Movies";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-
-  const [popular , setPopular] = useState([])
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
 
   useEffect(() => {
-    fetchPopular()
-  },[])
+    fetchPopular();
+  }, []);
 
   const fetchPopular = async () => {
-    const data = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=bfcf8c712c1faff84ccf1710a0da049e&language=en-US&page=1");
-    const movies = await data.json()
-    setPopular(movies.results)
-  }
-  
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=bfcf8c712c1faff84ccf1710a0da049e&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
   return (
     <div className="App">
-          <div className="popular-movies">
-            {popular.map((movie) => {
-              return <Movies key={movie.id} movie={movie}/>
-            })}
-          </div>
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="popular-movies">
+        <AnimatePresence>
+          {filtered.map((movie) => {
+            return <Movies key={movie.id} movie={movie} />;
+          })}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import './Movies.css'
+import React from "react";
+import "./Movies.css";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Movies = ({movie}) => {
+const Movies = ({ movie }) => {
   return (
-    <div>
-        <h2>{movie.title}</h2>
-        <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt="" />
-        <div className="info">
-        <p>IMDB:<span>{movie.vote_average}</span></p>
-        </div>
-        
-    </div>
-  )
-}
+    <motion.div
+      layout
+      animate={{ opacity: 1,scale:1 }}
+      initial={{ opacity: 0,scale:0 }}
+      exit={{ opacity: 0,scale:0 }}
+    >
+      <h2>{movie.title}</h2>
+      <img
+        src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+        alt=""
+      />
+      <div className="info">
+        <p>
+          IMDB:<span>{movie.vote_average}</span>
+        </p>
+      </div>
+    </motion.div>
+  );
+};
 
-export default Movies
+export default Movies;
